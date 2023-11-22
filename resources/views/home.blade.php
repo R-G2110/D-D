8bb47dcc8d5455896b25baedc46e0f11d985e78c
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-11-22 17:57:41 +0100

    [ADD]: Cards characters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@extends('layouts.main')
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
+@section('content')
 
-</head>
-<body>
-    <h1>test</h1>
-</body>
-</html>
+    <div class="container mt-5">
+        <h1 class="text-center mb-4"> D&D Characters</h1>
+        <div class="row">
+            @foreach ($characters as $character)
+                <div class="col-4 h-100 mb-4">
+                    <div class="card"">
+                        {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                        <div class="card-body">
+                        <h5 class="card-title">Nome: {{$character->name}}</h5>
+                        <p class="card-text">Background: {{$character->background}}</p>
+                        <a href="#" class="btn btn-primary">Più info</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+@endsection
+
+@section('title')
+    | Homepage
+@endsection
