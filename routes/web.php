12677ca4ded6b1aca4c358ef1a09e93487ae9055
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-12-05 16:32:17 +0100

    [ADD]: laravel 10

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\CharacterController;
-use App\Models\Character;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,11 +11,27 @@ use App\Models\Character;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
 
 Route::get('/', [PageController::class, "index"])->name("home");
 
 Route::resource('characters', CharacterController::class);
+
+
+require __DIR__.'/auth.php';
