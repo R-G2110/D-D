7bc58c0c51a165cd2316717de6feeb6d2bf8e466
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-12-11 17:28:43 +0100

    [ADD]: seeder skill

---
+++ database/seeders/SkillsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Skill;
+use Faker\Generator as Faker;
+use App\Functions\Helper;
+
+class SkillsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<30; $i++){
+            $skill = new Skill();
+            $skill->name = $faker->word(30);
+            $skill->description = $faker->paragraph();
+            $skill->characteristic = $faker->words(30,true);
+            $skill->slug = Helper::generateSlug($skill->name, Skill::class);
+
+            $skill->save();
+        }
+    }
+}
