67c5c37b2cb1bc8d4a2568cddf7941a51a4f4795
Melozzo <carmecamma1995@gmail.com>
2023-12-15 19:02:43 +0100

    [fix]: style page

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,34 +1,32 @@
-<header>
-    <header>
-        <nav >
-            <ul class="nav flex justify-content-center menu">
-                <li class="nav-item">
-                  <a class="nav-link change" href="{{ route('admin.home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link change" href="{{route('admin.characters.index')}}">Lista personaggi</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link change" href="{{route('admin.characters.create')}}">Crea personaggio</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle change" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Altro
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a></li>
-                      <li><a class="dropdown-item" href="{{ url('profile') }}">{{__('Profilo')}}</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                      </a></li>
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                      </form>
-                    </ul>
-                  </li>
-              </ul>
-        </nav>
-    </header>
+ <header>
+    <nav class="">
+        <ul class="nav flex justify-content-center align-items-center menu">
+            <li class="nav-item mx-2 ">
+                <a class="nav-link change" href="{{ route('admin.home') }}">Home</a>
+            </li>
+            <li class="nav-item mx-2 ">
+                <a class="nav-link change" href="{{route('admin.characters.index')}}">Lista personaggi</a>
+            </li>
+            <li class="nav-item mx-2 ">
+                <a class="nav-link change" href="{{route('admin.characters.create')}}">Crea personaggio</a>
+            </li>
+            <li class="nav-item dropdown mx-2">
+                <a class="nav-link dropdown-toggle change" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Altro
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a></li>
+                    <li><a class="dropdown-item" href="{{ url('profile') }}">{{__('Profilo')}}</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                    </a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </ul>
+            </li>
+        </ul>
+    </nav>
 </header>
