04e7193fed53f0e13a4eaf507f25577415410061
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-15 16:35:53 +0100

    [ add ] race to show

--- resources/views/admin/characters/show.blade.php
+++ resources/views/admin/characters/show.blade.php
@@ -1,38 +1,37 @@
 @extends('layouts.admin')
 
 @section('content')
-
     <div class="container mt-5">
-        <h1 class="text-center">Info {{$character->name}}</h1>
+        <h1 class="text-center">Info {{ $character->name }}</h1>
         <div class="card justify-content-center w-50">
-            <img src="{{$character->image}}" class="card-img-top" alt="...">
+            <img src="{{ $character->image }}" class="card-img-top" alt="...">
             <div class="card-body">
-                <p class="card-text">Background: {{$character->background}}</p>
-                <p class="card-text">Altezza: {{$character->height}} ,
-                    Peso: {{$character->weight}}</p>
-                <p class="card-text">Classe Armatura: {{$character->armor_class}}</p>
-                <h4 class="card-text"> FOR: {{$character->FOR}}</h4>
-                <h4 class="card-text"> DES: {{$character->DES}}</h4>
-                <h4 class="card-text"> COS: {{$character->COS}}</h4>
-                <h4 class="card-text"> INT: {{$character->INT}}</h4>
-                <h4 class="card-text"> SAG: {{$character->SAG}}</h4>
-                <h4 class="card-text"> CAR: {{$character->CAR}}</h4>
+                <p class="card-text">Background: {{ $character->background }}</p>
+                <p class="card-text">Altezza: {{ $character->height }} ,
+                    Peso: {{ $character->weight }}</p>
+                <p class="card-text">Classe Armatura: {{ $character->armor_class }}</p>
+                <p><strong>Race:</strong> {{ $character->race?->name }}</p>
+                <h4 class="card-text"> FOR: {{ $character->FOR }}</h4>
+                <h4 class="card-text"> DES: {{ $character->DES }}</h4>
+                <h4 class="card-text"> COS: {{ $character->COS }}</h4>
+                <h4 class="card-text"> INT: {{ $character->INT }}</h4>
+                <h4 class="card-text"> SAG: {{ $character->SAG }}</h4>
+                <h4 class="card-text"> CAR: {{ $character->CAR }}</h4>
 
 
-                <a href="{{ route('admin.characters.edit', $character)}}" class="btn btn-warning">Modifica</a>
+                <a href="{{ route('admin.characters.edit', $character) }}" class="btn btn-warning">Modifica</a>
 
-                <form class="delete-btn d-inline-block" action="{{ route('admin.characters.destroy', $character) }}" method="POST"
-                    onsubmit="return confirm('Sei sicuro di voler eliminare {{$character->name}}?')">
+                <form class="delete-btn d-inline-block" action="{{ route('admin.characters.destroy', $character) }}"
+                    method="POST" onsubmit="return confirm('Sei sicuro di voler eliminare {{ $character->name }}?')">
                     @csrf
                     @method('DELETE')
-                    <button type="submit"  class="btn btn-danger"><i class="fa-regular fa-trash-can"></i> Delete</button>
+                    <button type="submit" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i> Delete</button>
                 </form>
 
+            </div>
         </div>
-    </div>
+    @endsection
 
-@endsection
-
-@section('title')
-    | Homepage
-@endsection
+    @section('title')
+        | Homepage
+    @endsection
